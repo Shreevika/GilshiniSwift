102060672fdfb31dd83adb2e910a8733ed239cd7
Shreevika <k.m.g.shreevika@gmail.com>
2024-03-16 14:30:25 +0530

    Initial Commit

---
+++ GilshiniSwift/GilshiniSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GilshiniSwiftApp.swift
+//  GilshiniSwift
+//
+//  Created by NIBM on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct GilshiniSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
